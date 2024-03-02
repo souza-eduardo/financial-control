40eeac50cd80502818b07a7b80801b43e50a7d2e
souza-eduardo <esouzasantos085@gmail.com>
2024-03-01 21:41:03 -0400

    cria o header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,11 @@
+import * as C from './styles'
+
+const Header = () => {
+  return (
+    <C.Header>
+      <C.Title>Controle Financeiro</C.Title>
+    </C.Header>
+  )
+}
+
+export default Header
---
+++ src/components/Header/styles.ts
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const Header = styled.header`
+  background-color: #473BF0;
+  text-align: center;
+  height: 140px;
+`;
+
+export const Title = styled.h1`
+  font-size:2.3rem;
+  padding-top: 30px;  
+  color: white;
+`;
