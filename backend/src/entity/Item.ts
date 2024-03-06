4a632005f5659130263af1cd5fb6d1def32e8ff6
souza-eduardo <esouzasantos085@gmail.com>
2024-03-05 21:33:13 -0400

    database connection

--- backend/src/entity/Item.ts
+++ backend/src/entity/Item.ts
@@ -1,1 +1,16 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm"
 
+@Entity()
+export class Item {
+  @PrimaryGeneratedColumn()
+  id: number
+
+  @Column()
+  category: string
+
+  @Column("float")
+  value: number
+
+  @Column()
+  type: string
+}
